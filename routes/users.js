9c295be020827433421829171330146e00937783
Mitchell Makos <mitchell.makos@ibm.com>
2016-11-20 19:12:57 +0000

    login route update

--- routes/users.js
+++ routes/users.js
@@ -5,7 +5,7 @@ var LocalStrategy = require('passport-local').Strategy;
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send('login');
 });
 
 module.exports = router;
