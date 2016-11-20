806aaf4c0c214062c5b361679bba04bd41d76100
Mitchell Makos <mitchell.makos@ibm.com>
2016-11-20 22:48:15 +0000

    route changes

--- routes/users.js
+++ routes/users.js
@@ -38,72 +38,36 @@ var newUser = {
         dev_id_6: null
 	};
 
-var createUser = function(newUser, company, name, title, email, phone, auth_token, username, password, dev_id_1, dev_id_2, dev_id_3, dev_id_4, dev_id_5, dev_id_6) {
+var createUser = function(newUser, req, res) {
 		
 		var user = newUser;
-		user.company = company;
-		user.name = name;
-		user.title = title;
-		user.email = email;
-		user.phone = phone;
-		user.auth_token = auth_token;
-		user.username = username;
-		user.password = password;
-		user.dev_id_1 = dev_id_1;
-		user.dev_id_2 = dev_id_2;
-		user.dev_id_3 = dev_id_3;
-		user.dev_id_4 = dev_id_4;
-		user.dev_id_5 = dev_id_5;
-		user.dev_id_6 = dev_id_6;
-					   			
-	db.insert(user, email, function(err, doc){
+		user.company = req.company;
+		user.name = req.name;
+		user.title = req.title;
+		user.email = req.email;
+		user.phone = req.phone;
+		user.auth_token = req.auth_token;
+		user.username = req.username;
+		user.password = req.password;
+		user.dev_id_1 = req.dev_id_1;
+		user.dev_id_2 = req.dev_id_2;
+		user.dev_id_3 = req.dev_id_3;
+		user.dev_id_4 = req.dev_id_4;
+		user.dev_id_5 = req.dev_id_5;
+		user.dev_id_6 = req.dev_id_6;			   			
+	db.insert(user, user.email, function(err, doc){
 		if(err) {
 			console.log(err);
-			response.sendStatus(500);
+			res.sendStatus(500);
 		} else
-			response.sendStatus(200);
-		response.end()
+			res.sendStatus(200);
+		res.end()
 	});
 }
 
 
-router.post('/users', function(req, res, next) { 
-	var company = req.body.company;
-	var name = req.body.name;
-	var title = req.body.title;
-	var email = req.body.email;
-	var phone = req.body.phone;
-	var auth_token = req.body.auth_token;
-	var username = req.body.username;
-	var password = req.body.password;
-	var dev_id_1 = req.body.dev_id_1;
-	var dev_id_2 = req.body.dev_id_2;
-	var dev_id_3 = req.body.dev_id_3;
-	var dev_id_4 = req.body.dev_id_4;
-	var dev_id_5 = req.body.dev_id_5;
-	var dev_id_6 = req.body.dev_id_6;
-   
-     // Form Validator
-     req.checkBody('company','Company Name field is required').notEmpty();
-     req.checkBody('name','Company Name field is required').notEmpty();
-     req.checkBody('title','Company Name field is required').notEmpty();
-     req.checkBody('email','Company Name field is required').isEmail();
-	 req.checkBody('company','Company Name field ').notEmpty();
-	 req.checkBody('auth_token','Auth Token field iis requireds required').notEmpty();
-	 req.checkBody('username','Username field is required').notEmpty();
-	 req.checkBody('password','Password field is required').notEmpty();
-	 req.checkBody('password2','Passwords do not match').equals(req.body.password);
-	 req.checkBody('dev_id_1','You need to register at least 1 device').notEmpty();
-	 
-	 // Check Errors
-	  var errors = req.validationErrors();
-	
-	  if(errors){
-	  	res.render('register', {
-	  		errors: errors
-	  	});
-	  } else{
-	  createUser(null, company, name, title, email, phone, auth_token, username, password, dev_id_1, dev_id_2, dev_id_3, dev_id_4, dev_id_5, dev_id_6, res);	
+router.post('/register', function(req, res, next) { 
+	  createUser();	
       req.flash('sucess', 'You are now registered and can login');
       res.location('/');
       res.redirect('/');
