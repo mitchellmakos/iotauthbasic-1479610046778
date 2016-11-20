af4ecf0c9b1827ea8a7053f57904b2a8682fe8b9
Mitchell Makos <mitchell.makos@ibm.com>
2016-11-20 03:32:14 +0000

    initial commit

--- app.js
+++ app.js
@@ -7,6 +7,7 @@
 // This application uses express as its web server
 // for more info, see: http://expressjs.com
 var express = require('express');
+var path = require('path');
 
 // cfenv provides access to your Cloud Foundry environment
 // for more info, see: https://www.npmjs.com/package/cfenv
@@ -15,11 +16,20 @@ var cfenv = require('cfenv');
 // create a new express server
 var app = express();
 
+var index = require('./routes/index');
+
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 // serve the files out of ./public as our main files
 app.use(express.static(__dirname + '/public'));
 
+
+
 // get the app environment from Cloud Foundry
 var appEnv = cfenv.getAppEnv();
+
+app.use('/', index);
 
 // start server on the specified port and binding host
 app.listen(appEnv.port, '0.0.0.0', function() {
