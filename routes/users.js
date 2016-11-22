2a142e74237bed912a60d0f985a25c9f09020efd
Mitchell Makos <mitchell.makos@ibm.com>
2016-11-22 14:50:19 +0000

    included user details

--- routes/users.js
+++ routes/users.js
@@ -27,11 +27,30 @@ router.get('/register', function(req, res, next) {
 router.post('/register', function(req, res) { 
 var company=req.body.company;
 var name=req.body.name;
-
+var email = req.body.email;
+var auth_token = req.body.auth_token;
+var username = req.body.username;
+var password = req.body.password;
+var dev_id_1 = req.body.dev_id_1;
+var dev_id_2 = req.body.dev_id_2;
+var dev_id_3 = req.body.dev_id_3;
+var dev_id_4 = req.body.dev_id_4;
+var dev_id_5 = req.body.dev_id_5;
+var dev_id_6 = req.body.dev_id_6;	
 
 	db.insert({
 		"company": company,
-		"name": name}, null, function(err, body){
+		"name": name,
+		"email": email,
+		"auth_token": auth_token,
+		"uaername": username,
+		"password": password,
+		"dev_id_1": dev_id_1,
+		"dev_id_2": dev_id_2,
+		"dev_id_3": dev_id_3,
+		"dev_id_4": dev_id_4,
+		"dev_id_5": dev_id_5,
+		"dev_id_6": dev_id_6}, null, function(err, body){
 			if(!err){
 				console.log(body);
 			}
